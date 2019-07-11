72a07be8a5b6c9f83603c1d2b2e798e4d09ef483
LarissaZanata <zanatalarissamorais@gmail.com>
2019-07-10 22:55:54 -0300

    Trocando UsuarioBean de pacote

---
+++ CadastroDeTitulos/src/java/br/uem/bean/UsuarioBean.java
@@ -0,0 +1,109 @@
+package br.uem.bean;
+
+
+import java.io.Serializable;
+import javax.enterprise.context.SessionScoped;
+import javax.faces.application.FacesMessage;
+import javax.faces.context.FacesContext;
+import javax.inject.Named;
+import br.uem.dao.DaoLogin;
+
+@Named(value = "usuarioBean")
+@SessionScoped
+public class UsuarioBean implements Serializable{
+    private String usuario;
+    private String senha;
+    private String confirmaSenha;
+    private String nome;
+    private String adm;
+    
+    private DaoLogin daoLogin = new DaoLogin();
+
+    
+    public UsuarioBean(){
+    
+    }
+
+    public String getSenha() {
+        return senha;
+    }
+
+
+    public void setSenha(String senha) {
+        this.senha = senha;
+    }
+
+    public String getConfirmaSenha() {
+        return confirmaSenha;
+    }
+
+    public void setConfirmaSenha(String confirmaSenha) {
+        this.confirmaSenha = confirmaSenha;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+
+    public String getAdm() {
+        return adm;
+    }
+
+
+    public void setAdm(String adm) {
+       this.adm = adm;
+    }
+    
+    public String getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(String usuario) {
+        this.usuario = usuario;
+    }
+    
+    
+    public String novoUsuario() throws Exception{
+        boolean cadastrado = false;
+        
+        if(verificaSenhas(this.getSenha(), this.getConfirmaSenha())){
+            cadastrado = daoLogin.cadastrarFuncionario(this.getUsuario(), this.getSenha(), this.getAdm(), this.getNome());
+        }else{
+            FacesMessage msg = new FacesMessage("Senhas diferentes!");
+            FacesContext.getCurrentInstance().addMessage("erro", msg); 
+        }
+        
+        
+        //verifica de cadastro deu certo
+        if(cadastrado){
+            FacesMessage msg = new FacesMessage("Cadastrado com Sucesso!");
+            FacesContext.getCurrentInstance().addMessage("sucesso", msg);
+            Thread.sleep(1000);
+            return "/index";
+        }else{
+            FacesMessage msg = new FacesMessage("Erro ao cadastrar usuário!");
+            FacesContext.getCurrentInstance().addMessage("erro", msg);
+           return null; 
+        }
+        
+    }
+    
+    private boolean verificaSenhas(String senha1, String senha2){
+        if(senha1.equals(senha2)){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
+
+    public String CancelaCadastro(){
+        return "/index";
+    }
+    
+}
